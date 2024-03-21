bd1bced4440eb1bbd4f8a00f01ff3f1cc5100bbc
Mansoor-GH23 <mansoor.ul.hasan@hotmail.com>
2024-03-21 10:52:29 -0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DevOps_Demo1.Models;
+using DevOps_Demo1.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+namespace DevOps_Demo1.Controllers
+{
+    public class HomeController : Controller
+    {
+        IPostRepository postRepository;
+        public HomeController(IPostRepository _postRepository)
+        {
+            postRepository = _postRepository;
+        }
+        public IActionResult Index()
+        {
+            var model = postRepository.GetPosts();
+            return View(model);
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+            return View();
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/Repository/IPostRepository.cs
@@ -0,0 +1,9 @@
+﻿using DevOps_Demo1.Models;
+using System.Collections.Generic;
+namespace DevOps_Demo1.Repository
+{
+    public interface IPostRepository
+    {
+        List<PostViewModel> GetPosts();
+    }
+}
